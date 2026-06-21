c84612fa978e0461327ca8dd4eb1f43ad9d24e64
Georg Ostertag <georg@ostertage.de>
2026-06-21 18:38:08 +0200

    fix real DNS names

--- dev/app/smartthingsbridge/settings.py
+++ dev/app/smartthingsbridge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)#vje)ornntlaedt(nb###yxlx23(oseg$wxajy7q+(ob!ls7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["smartthingsbridge.ostertage.de","localhost"]
 
 
 # Application definition
